040fa8fe5f2a40fd06d51d5d549fc1981cf1d689
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-03-29 07:16:32 +0100

    popular of the week section not figma design yet

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 const API_URL = 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=eab119f4519b3c48189fd1039aea8fed&language=en-US&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=eab119f4519b3c48189fd1039aea8fed&language=en-US&query="'
+const API_POPULAR = 'https://api.themoviedb.org/3/movie/popular?api_key=eab119f4519b3c48189fd1039aea8fed&language=en-US&page=1'
+const API_UPCOMING = 'https://api.themoviedb.org/3/movie/upcoming?api_key=eab119f4519b3c48189fd1039aea8fed&language=en-US&page=1'
 
 const form = document.getElementById('form')
 const search = document.querySelector('.search')
@@ -40,6 +41,7 @@ const options = {
 }
 
 getMovies(API_URL)
+getPopularMovies(API_POPULAR)
 
 async function getMovies(url) {
   const res = await fetch(url, options)
@@ -47,6 +49,14 @@ async function getMovies(url) {
 
   displayMovies(data.results)
 }
+
+async function getPopularMovies(url) {
+  const res = await fetch(url, options)
+  const data = await res.json()
+
+  displayPopularMovies(data.results)
+}
+
 let countItem = 0
 let itemActive = 0
 function showSlider() {
@@ -131,3 +141,40 @@ function displayMovies(movies) {
   })
 }
 
+//Popular section
+function displayPopularMovies(movies) {
+  const popularList = document.querySelector('.movie-container')
+  if(!popularList){
+    console.error('Popular list element not found in html')
+  }
+  popularList.innerHTML = ''
+  if (!Array.isArray(movies)) {
+    console.error("Movies is not an array:", movies);
+    return;
+  }
+
+  if (!movies || movies.length === 0) {
+    console.warn("Movies array is empty");
+    return;
+  }
+
+  movies.forEach((movie, index) => {
+    const movieItem = document.createElement('div')
+    movieItem.classList.add('movie-card')
+
+    movieItem.innerHTML = `
+      <div class="movie-poster">
+        <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}" >
+      </div>
+      <div class="movie-info">
+        <h3 class="movie-title">${movie.title}</h3>
+        <p class="movie-rating">
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+          </svg> 
+        ${movie.vote_average}</p>
+      </div>
+    `
+    popularList.appendChild(movieItem)
+  })
+}
