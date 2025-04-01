bbc2b07e9851bd1fcac6077c70c6fc0abf04a4ce
https://github.com/MudehDieudonne <mukummudeh@gmail.com>
2025-04-01 18:48:51 +0200

    Added like section with bookmark

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const prev = document.getElementById('prev')
 let items 
 let thumbnails
 let watchList = JSON.parse(localStorage.getItem('watchList')) || []
+let likedMovies = JSON.parse(localStorage.getItem('likedMovie')) || []
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -182,6 +183,7 @@ function displayPopularMovies(movies) {
     const movieItem = document.createElement('div')
     movieItem.classList.add('movie-card')
     const isFavorite = watchList.some(favMovie => favMovie === movie.id)
+    const isLiked = Array.isArray(likedMovies) && likedMovies.some(lkMovie => lkMovie === movie.id);
 
     movieItem.innerHTML = `
       <div class="movie-poster">
@@ -196,20 +198,28 @@ function displayPopularMovies(movies) {
           ${movie.vote_average}
         </p>
         <button class="add-to-watchlist-btn ${isFavorite ? 'active' : ''}" data-movie-id=${movie.id}>
-          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m17 21-5-4-5 4V3.889a.92.92 0 0 1 .244-.629.808.808 0 0 1 .59-.26h8.333a.81.81 0 0 1 .589.26.92.92 0 0 1 .244.63V21Z"/>
-          </svg>
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z"/>
+        </svg>
         </button>
       </div>
+      <button class="like-icon ${isLiked ? 'like-icon-active' : ''}" data-movie-id=${movie.id}>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"/>
+        </svg>
+      </button>
     `
     popularList.appendChild(movieItem)
   })
   addToWatchlistButtons(movies)
+  addToLikeButtons(movies)
 }
 
 //Upcoming Section
 function displayUpcomingMovies(movies) {
   const upcomingList = document.querySelector('.upcoming-movie-container')
+  const isFavorite = watchList.some(favMovie => favMovie === movies.id)
+  const isLiked = Array.isArray(likedMovies) && likedMovies.some(lkMovie => lkMovie === movies.id);
 
   upcomingList.innerHTML = ''
 
@@ -232,7 +242,17 @@ function displayUpcomingMovies(movies) {
           </svg>
           ${movie.release_date}
         </p>
+          <button class="add-to-watchlist-btn ${isFavorite ? 'active' : ''}" data-movie-id=${movie.id}>
+            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M7.833 2c-.507 0-.98.216-1.318.576A1.92 1.92 0 0 0 6 3.89V21a1 1 0 0 0 1.625.78L12 18.28l4.375 3.5A1 1 0 0 0 18 21V3.889c0-.481-.178-.954-.515-1.313A1.808 1.808 0 0 0 16.167 2H7.833Z"/>
+            </svg>
+          </button>
       </div>
+      <button class="like-icon ${isLiked ? 'like-icon-active' : ''}" data-movie-id=${movie.id}>
+        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"/>
+        </svg>
+      </button>
     `
     upcomingList.appendChild(movieItem)
   })
@@ -263,6 +283,35 @@ function addToWatchlistButtons(movies){
   })
 }
 
+//add to liked list
+function addToLikeButtons(movies){
+  const addLikeBtn = document.querySelectorAll('.like-icon')
+  addLikeBtn.forEach(button => {
+    button.addEventListener('click', () => {
+      const movieId = parseInt(button.dataset.movieId)
+      console.log(movieId)
+      const movieToAdd = movies.find(movie => movie.id === movieId)
+
+      if(movieId) {
+        if(Array.isArray(likedMovies)){
+          if(likedMovies.some(movie => movie.id === movieId)){
+            //remove from liked list
+            likedMovies = likedMovies.filter(movie => movie.id !== movieId)
+            button.classList.remove('like-icon-active')
+          } else {
+            likedMovies = likedMovies.push(movieToAdd)
+            button.classList.add('like-icon-active')
+          }
+          localStorage.setItem('likedMovie', JSON.stringify(likedMovies))
+          updateLikelistUI()
+        }else {
+          console.log(`${likedMovies.length} is not an array`)
+        }
+      }
+    })
+  })
+}
+
 //Update Watchlist UI
 function updateWatchlistUI() {
   const watchListContainer = document.querySelector('.watchlist-movie-container')
@@ -288,5 +337,32 @@ function updateWatchlistUI() {
       </div>
     `
     watchListContainer.appendChild(movieItem)
+  })
+}
+
+function updateLikelistUI() {
+  const likeListContainer = document.querySelector('.your-likes-container')
+  likeListContainer.innerHTML = ''
+
+  likedMovies.forEach(movie => {
+    const movieItem = document.createElement('div')
+    movieItem.classList.add('movie-card')
+    movieItem.innerHTML = `
+      <div class="movie-poster">
+        <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}">
+      </div>
+      <div class="movie-info">
+        <h3 class="movie-title">
+          ${movie.title}
+        </h3>
+        <p class="movie-rating">
+          <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z"/>
+          </svg>
+          ${movie.vote_average}
+        </p>
+      </div>
+    `
+    likeListContainer.appendChild(movieItem)
   })
 }
